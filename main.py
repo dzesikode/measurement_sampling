fa7186523b8a1ce663cda0b8071e0cd5058f5fef
dzesikode <jban@sdf.org>
2024-08-01 10:33:43 +0300

    Rename variable

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from src.sample_measurements import sampleMeasurements
 
 
 if __name__ == "__main__":
-    measurements = [
+    exampleMeasurements = [
         Measurement(datetime(2017, 1, 3, 10, 4, 45), MeasType.TEMP, 35.79),
         Measurement(datetime(2017, 1, 3, 10, 1, 18), MeasType.SPO2, 98.78),
         Measurement(datetime(2017, 1, 3, 10, 9, 7), MeasType.TEMP, 35.01),
@@ -14,19 +14,9 @@ if __name__ == "__main__":
         Measurement(datetime(2017, 1, 3, 10, 5, 1), MeasType.SPO2, 95.08),
     ]
     startTime = datetime(2017, 1, 3, 10, 0, 0)
-    output = sampleMeasurements(startTime, measurements)
+    output = sampleMeasurements(startTime, exampleMeasurements)
 
-    expected = {
-        MeasType.TEMP: [
-            {datetime(2017, 1, 3, 10, 5): measurements[0]},
-            {datetime(2017, 1, 3, 10, 10): measurements[2]},
-        ],
-        MeasType.SPO2: [
-            {datetime(2017, 1, 3, 10, 5): measurements[-2]},
-            {datetime(2017, 1, 3, 10, 10): measurements[-1]},
-        ],
-    }
     for type, intervals in output.items():
         for interval in intervals:
-            for interval_key, measurement in interval.items():
-                print(interval_key, type, measurement.value)
+            for intervalKey, measurement in interval.items():
+                print(intervalKey, type, measurement.value)
